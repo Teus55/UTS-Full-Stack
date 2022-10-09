3b65dfaa1c410e8e409dae01f03f8951b1250449
Teus55 <115389214+Teus55@users.noreply.github.com>
2022-10-09 16:30:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+require_once('class/mahasiswa.php');
+require_once('class/jadwal.php');
+require_once('class/jamkuliah.php');
+$objMahasiswa = new mahasiswa();
+$objJamkuliah = new jamkuliah();
+$objJadwal = new Jadwal();
+if (isset($_POST['btnsubmit'])) {
+    $nrp = $_POST['selMahasiswa'];
+} else {
+    $nrp = null;
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,40 +25,71 @@
 <body>
     <form method="post">
         <p><label for="">Mahasiswa : </label>
-            <select id="selMahasiswa">
+            <select id="selMahasiswa" name="selMahasiswa">
                 <option value="">--Pilih Mahasiswa--</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
+                <?php
+                $res = $objMahasiswa->getMahasiswa();
+                while ($row = $res->fetch_assoc()) {
+                    if ($row['nrp'] != $nrp) {
+                        echo "<option value='" . $row['nrp'] . "'>" . $row['nama'] . "</option>";
+                    } else {
+                        echo "<option value='" . $row['nrp'] . "'selected>" . $row['nama'] . "</option>";
+                    }
+                }
+                ?>
             </select>
             <input type="submit" name="btnsubmit" value="Pilih Mahasiswa">
         </p>
-        <table border="1" style="min-width: 500px; max-width: 1000px;">
-            <th>jam</th>
-            <th>Minggu</th>
-            <th>Senin</th>
-            <th>Selasa</th>
-            <th>Rabu</th>
-            <th>Kamis</th>
-            <th>Jumat</th>
-            <th>Sabtu</th>
-            <?php
-            for ($i = 0; $i <= 7; $i++) {
-                echo "<tr><td>jam</td>";
-                echo "<td></td>";
-                echo "<td></td>";
-                echo "<td></td>";
-                echo "<td></td>";
-                echo "<td></td>";
-                echo "<td></td>";
-                echo "<td></td></tr>";
+    </form>
+    <table border="1" style="min-width: 500px; max-width: 1000px;">
+        <th>jam</th>
+        <th>Minggu</th>
+        <th>Senin</th>
+        <th>Selasa</th>
+        <th>Rabu</th>
+        <th>Kamis</th>
+        <th>Jumat</th>
+        <th>Sabtu</th>
+        <?php
+        $arr = array(
+            array(false, false, false, false, false, false, false,),
+            array(false, false, false, false, false, false, false,),
+            array(false, false, false, false, false, false, false,),
+            array(false, false, false, false, false, false, false,),
+            array(false, false, false, false, false, false, false,),
+            array(false, false, false, false, false, false, false,),
+            array(false, false, false, false, false, false, false,),
+            array(false, false, false, false, false, false, false,),
+            array(false, false, false, false, false, false, false,),
+            array(false, false, false, false, false, false, false,),
+            array(false, false, false, false, false, false, false,),
+            array(false, false, false, false, false, false, false,)
+        );
+        $res2 = $objJadwal->getJadwal($nrp);
+        $res3 = $objJamkuliah->getJam();
+        while ($row = $res2->fetch_assoc()) {
+            $arr[$row['idjam_kuliah']-1][$row['idhari']-1] = true;
+        }
+        $i = 0;
+        while ($row2 = $res3->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row2["jam_mulai"] . "-" . $row2["jam_selesai"] . "</td>";
+            for ($j = 0; $j <= 6; $j++) {
+                if ($arr[$i][$j] == true) {
+                    echo "<td>test</td>";
+                } else {
+                    echo "<td></td>";
+                }
             }
-            ?>
-        </table>
-    </form>
-    <form method="post" action="inputjadwal.php">
-        <input type="submit" name="btnsubmit" value="Ubah Jadwal">
-    </form>
+            echo "</tr>";
+            $i++;
+        }
+        ?>
+    </table><br>
+    <?php
+    echo "<a href='inputjadwal.php?nrp=$nrp'><button>Ubah Jadwal</button></a>";
+    ?>
+
 </body>
 
 </html>
