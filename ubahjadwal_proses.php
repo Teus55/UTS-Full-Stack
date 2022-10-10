0655776bd6bdea675ac29030b9a89dfc264dfabe
Teus55 <115389214+Teus55@users.noreply.github.com>
2022-10-10 19:15:42 +0700

    Update ubahjadwal_proses.php

--- ubahjadwal_proses.php
+++ ubahjadwal_proses.php
@@ -3,15 +3,16 @@ require_once('class/jadwal.php');
 $objJadwal = new Jadwal();
 if (isset($_POST['btnsimpan'])) {
     if (isset($_POST["nrp"])) {
-
-
         $nrp = $_POST["nrp"];
-
-        $objJadwal->deleteData($nrp);
-        foreach ($_POST["chk"] as $chk) {
-            $jam = (explode(".", $chk)[0]) + 1;
-            $hari = (explode(".", $chk)[1]) + 1;
-            $res = $objJadwal->insertJadwal($nrp, $jam, $hari);
+        if (!isset($_POST["chk"])) {
+            header("location:ubahjadwal.php?nrp=" . $nrp . "&error=1");
+        } else {
+            $objJadwal->deleteData($nrp);
+            foreach ($_POST["chk"] as $chk) {
+                $jam = (explode(".", $chk)[0]) + 1;
+                $hari = (explode(".", $chk)[1]) + 1;
+                $res = $objJadwal->insertJadwal($nrp, $jam, $hari);
+            }
         }
     }
 }
